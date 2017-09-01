9cbd7678837c6c43f3a9fe66db4ac978f3bfccaf
Jagodowski <jago425@users.noreply.github.com>
2017-09-01 12:17:46 -0400

    added more game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }else {
+    alert("Sorry, try again");
+  }
+}
 
+//console.log("User flipped" + cardOne);
+//console.log("User flipped" + cardThree);
