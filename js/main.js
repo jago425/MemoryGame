6920eee4c8689e06bcf16b37062d023910d25bae
Jagodowski <jago425@users.noreply.github.com>
2017-09-05 10:44:09 -0400

    added more game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-console.log("Up and running");
+//console.log("Up and running");
 //var cardOne = "queen";
 //var cardTwo = "queen";
 //var cardThree = "king";
 //var cardFour = "king";
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2){
+var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+    console.log("You found a match!");
   }else {
-    alert("Sorry, try again");
+    console.log("Sorry, try again");
   }
 }
+var flipCard = function(cardId){
+  cards[cardId];
+  cardsInPlay.push(cards[cardId]);
+  //var cardOne = cards[0];
+  //cardsInPlay.push(cardOne);
+  //console.log("user flipped" + " " + cardOne)
+  console.log("user flipped" + " " + (cards[cardId]));
+  //var cardTwo = cards[2];
+  //cardsInPlay.push(cardTwo);
+  //console.log("user flipped king");
+  if (cardsInPlay.length === 2){
+  }
+checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+
 
 //console.log("User flipped" + cardOne);
 //console.log("User flipped" + cardThree);
